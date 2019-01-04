756c4ad3187ba319974673bb38e190c4da34e82f
Basar Sen <basarsen@hotmail.com>
2019-01-04 13:16:07 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-   res.send('Okay 123');
+   res.send('Up and running!');
 });
 
-app.listen(3000, () => {
-   console.log('Port 3000 started');
+const port = process.env.PORT;
+
+app.listen(port, () => {
+   console.log('Port ' + port + ' started');
 });
