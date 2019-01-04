3a9142ebad4db403b7996f653e56460636a9ff17
Basar Sen <basarsen@hotmail.com>
2019-01-04 13:03:02 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+   res.send('Okay 123');
+});
+
+app.listen(3000, () => {
+   console.log('Port 3000 started');
+});
